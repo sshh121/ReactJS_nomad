19d1f6612896f542030f8815370fc7c6495bd5a2
sshh121 <lc307715@gmail.com>
2023-01-16 01:10:44 +0900

    Effect_finish

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,13 +1,28 @@
-import Button from "./Button"
-import styles from "./App.module.css"
+import { useEffect, useState } from "react"
+
+function Hello() {
+  // useEffect(function() {
+  //   console.log("hi :)")
+  //   return function() {
+  //     console.log("bye :(")
+  //   }
+  // }, [])
+  useEffect(() => {
+    console.log("hi :)")
+    return () => console.log("bye :(")
+  }, [])
+  return <h1>Hello</h1>
+}
 
 function App() {
-  return (
-    <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
-    </div>
-  );
+ const [showing, setShowing] = useState(false)
+ const onClick = () => setShowing((prev) => !prev)
+ return (
+  <div>
+    {showing ? <Hello/> : null}
+    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+  </div>
+ )
 }
 
 export default App;
