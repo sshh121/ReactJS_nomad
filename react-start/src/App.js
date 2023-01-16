632345573bbf558151f5fe29a785719427459134
sshh121 <lc307715@gmail.com>
2023-01-17 00:43:17 +0900

    0116_practice

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,28 +1,43 @@
 import { useEffect, useState } from "react"
 
-function Hello() {
-  // useEffect(function() {
-  //   console.log("hi :)")
-  //   return function() {
-  //     console.log("bye :(")
-  //   }
-  // }, [])
-  useEffect(() => {
-    console.log("hi :)")
-    return () => console.log("bye :(")
-  }, [])
-  return <h1>Hello</h1>
-}
-
 function App() {
- const [showing, setShowing] = useState(false)
- const onClick = () => setShowing((prev) => !prev)
- return (
-  <div>
-    {showing ? <Hello/> : null}
-    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-  </div>
- )
+  const [loading, setLoading] = useState(true)
+  // 영화 리스트를 받는 배열을 state에 생성
+  const [movies, setMovies] = useState([])
+  // 기존의 처리 방식말고, async-await 방식 사용을 위한 함수 생성
+  const getMovies = async() => {
+    const json = await ( 
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json()
+    setMovies(json.data.movies)
+    setLoading(false)
+  }
+
+  // 처음에만 실행하겠다..
+  useEffect(() => {
+    getMovies()
+  }, [])
+  // console.log(movies)
+  return (
+    <div>
+      {loading ? (<h1>Loading...</h1>) : 
+        (<div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image}/>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map(g => <li key={g}>{g}</li>)}
+              </ul>
+            </div>
+          ))} 
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default App;
